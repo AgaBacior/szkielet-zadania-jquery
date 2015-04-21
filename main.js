6c8f636d51e4d1ae95c2f960a571cb3b18abe413
Agnieszka Bacior <agnieszka.bacior@gmail.com>
2015-04-21 20:57:25 +0200

    Add a div with content
    Add a div with content

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 $(function() {
+	
 	var click = 0;
 	var tablica = ['red', 'green', 'blue'];
 	var kolor = 0; 
-	$('button').on("click", function() {
+	$('#button1').on("click", function() {
 			
 			$('p').each(function(){
 				$(this).css('color', tablica[(click + kolor) % 3]);				
 				kolor++
 			});
 		click++	
+	});
+	
+	$('#button2').on("click", function() {		
+		var newDiv = $('<div><div style="float: left; margin-right: 5px;">Jak</div><div style="float: left; margin-right: 5px;">żyć</div><div style="float: left; margin-right: 5px;">panie</div><div style="float: left;">premierze?</div></div>');
+		newDiv.appendTo('body');
 	});
 });
